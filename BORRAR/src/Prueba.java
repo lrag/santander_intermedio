31f58a003d89a4a886f9ba9e11d7937eddafb873
Nombre de usuario <usuario@correo.es>
2022-04-25 11:36:26 +0200

    Primer commit

--- BORRAR/src/Prueba.java
+++
@@ -1,9 +0,0 @@
-import java.security.DomainCombiner;
-
-public class Prueba {
-
-	public static void main(String[] args) {
-		
-	}
-	
-}
---
+++ Ej15_Spring_JPA_BOOT/src/main/java/com/curso/Aplicacion.java
@@ -0,0 +1,30 @@
+package com.curso;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.curso.modelo.entidad.Cliente;
+import com.curso.modelo.negocio.GestorClientes;
+
+@SpringBootApplication
+public class Aplicacion {
+
+	public static void main(String[] args) {
+
+		ApplicationContext appCtx = SpringApplication.run(Aplicacion.class, args);
+		
+		GestorClientes gc = appCtx.getBean(GestorClientes.class);
+		
+		Cliente cli = new Cliente(null,"Maggie","C/Evergreen Terrace","555",5000);
+		gc.insertar(cli);
+		
+		System.out.println("================================================");
+		List<Cliente> clientes = gc.listar();
+		clientes.forEach(c -> System.out.println(c));		
+		
+	}
+
+}
